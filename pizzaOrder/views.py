844a3a7ff2819b53d1da44af843305230c882500
shefanaTues <kaloyan.georgieev@gmail.com>
2021-07-04 14:31:36 +0300

    Add almost all create and view views

--- pizzaOrder/views.py
+++ pizzaOrder/views.py
@@ -1,54 +1,85 @@
-from django.shortcuts import render
+from django.forms.models import model_to_dict
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .forms import OrderForm
+from .forms import OrderForm, FlourForm, SauceForm
 from .models import Order, Topping, Flour, Sauce
 import json
 from django.core.serializers.json import DjangoJSONEncoder
+from django.core import serializers
 
+
+def queryset_to_dicts(queryset):
+    models = list(queryset)
+    for model in models:
+        model = model_to_dict(model)
+    return models
 # Create your views here.
 def index(request):
     return render(request, "pizzaOrder/index.html")
 
-def create_order(request):
+def create_order(request):    
     if(request.method == "POST"):
         form = OrderForm(request.POST)
         if(form.is_valid):
             form.save()
     else:
         flours = Flour.objects.all()
-        flours_json = json.dumps(list(flours), cls=DjangoJSONEncoder)
-
+        flour_dicts = serializers.serialize('json', flours, cls=DjangoJSONEncoder)
+        
         sauces = Sauce.objects.all()
-        sauces_json = json.dumps(list(sauces), cls=DjangoJSONEncoder)
+        sauces_json = serializers.serialize('json', sauces, cls=DjangoJSONEncoder)
+
         context = {}
         order_form = OrderForm()
         context['order_form'] = order_form
-        context['flours_json'] = flours_json
+        context['flours_json'] = flour_dicts
         context['sauces_json'] = sauces_json
 
         return render(request, "pizzaOrder/create_order.html", context)
 
-def create_pizza(request):
+def add_topping(request):
     pass
 
-def create_topping(request):
-    pass
+def add_flour(request):
+    if(request.method == "POST"):
+        form = FlourForm(request.POST)
+        if(form.is_valid):
+            form.save()
+            return redirect("flour-view")
+    return render(request, "pizzaOrder/add_flour.html")
 
-def create_flour(request):
-    pass
-
-def create_sauce(request):
-    pass
+def add_sauce(request):
+    if(request.method == "POST"):
+        form = SauceForm(request.POST)
+        if(form.is_valid):
+            form.save()
+            return redirect("sauce-view")
+    return render(request, "pizzaOrder/add_sauce.html")
 
 
 def view_toppings(request):
-    pass
+    context = {}
+    toppings = Topping.objects.all()
+    if(len(toppings) == 0):
+        context['none'] = True
+    context['toppings'] = toppings
+    return render(request, "pizzaOrder/view_topping.html", context)
 
 def view_flours(request):
-    pass
+    context = {}
+    flours = Flour.objects.all()
+    if(len(flours) == 0):
+        context['none'] = True
+    context['flours'] = flours
+    return render(request, "pizzaOrder/view_flour.html", context)
 
 def view_sauces(request):
-    pass
+    context = {}
+    sauces = Sauce.objects.all()
+    if(len(sauces) == 0):
+        context['none'] = True
+    context['sauces'] = sauces
+    return render(request, "pizzaOrder/view_sauce.html", context)
 
 
 def delete_topping(request):
