bbdbeb6291291835b60b518c5fb6d9c5122b6fe4
shefanaTues <kaloyan.georgieev@gmail.com>
2021-07-05 20:23:30 +0300

    Remove redundant null property on pizza toppings field

--- pizzaOrder/models.py
+++ pizzaOrder/models.py
@@ -14,7 +14,7 @@ class Flour(models.Model):
     description = models.CharField(max_length=200, blank=True)
 
 class Pizza(models.Model):
-    toppings = models.ManyToManyField("Topping", blank=True, null=True)
+    toppings = models.ManyToManyField("Topping", blank=True)
     sauce = models.ForeignKey("Sauce", to_field="name", default="tomato", on_delete=models.CASCADE)
     dough_flour = models.ForeignKey("Flour", to_field="name", default="white", on_delete=models.CASCADE)
     order = models.ForeignKey("Order", on_delete=models.CASCADE, null=True)
